e0226df10da5e2d3f31eb1298a1f9a534cca72fe
ErhardRainer <erhard.rainer@googlemail.com>
2023-03-18 00:40:01 +0100

    initial Version

---
+++ Win_NotifyIcon_TXT/Win_NotifyIcon_TXT/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Win_NotifyIcon_TXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.Datei = new System.Windows.Forms.Label();
+            this.cmd_SelectFile = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipText = "     private void trayIcon_MouseEnter(object sender, EventArgs e)";
+            this.trayIcon.BalloonTipTitle = "twtwtrwtrwtr";
+            this.trayIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Win_NotifyIcon_TXT";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseEnter);
+            this.trayIcon.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseMove);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(79, 10);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(566, 20);
+            this.txt_Path.TabIndex = 1;
+            // 
+            // Datei
+            // 
+            this.Datei.AutoSize = true;
+            this.Datei.Location = new System.Drawing.Point(13, 13);
+            this.Datei.Name = "Datei";
+            this.Datei.Size = new System.Drawing.Size(32, 13);
+            this.Datei.TabIndex = 2;
+            this.Datei.Text = "Datei";
+            // 
+            // cmd_SelectFile
+            // 
+            this.cmd_SelectFile.Location = new System.Drawing.Point(651, 8);
+            this.cmd_SelectFile.Name = "cmd_SelectFile";
+            this.cmd_SelectFile.Size = new System.Drawing.Size(75, 23);
+            this.cmd_SelectFile.TabIndex = 3;
+            this.cmd_SelectFile.Text = "File";
+            this.cmd_SelectFile.UseVisualStyleBackColor = true;
+            this.cmd_SelectFile.Click += new System.EventHandler(this.cmd_SelectFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 40);
+            this.Controls.Add(this.cmd_SelectFile);
+            this.Controls.Add(this.Datei);
+            this.Controls.Add(this.txt_Path);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Win_NotifyIcon_TXT";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.Label Datei;
+        private System.Windows.Forms.Button cmd_SelectFile;
+    }
+}
+
---
+++ Win_NotifyIcon_TXT/Win_NotifyIcon_TXT/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Win_NotifyIcon_TXT
+{
+    public partial class Form1 : Form
+    {
+        private ContextMenuStrip menuStrip;
+        private bool isFlyoutVisible = false;
+        private DateTime lastExecutionTime = DateTime.MinValue;
+        private bool isFirstExecution = true;
+        private Timer timer = null;
+        string strFile = "C:\\_OneDrive\\Erhard Rainer\\Computer - Programmierung\\Win_NotifyIcon_TXT\\Test.txt";
+        int FontSize = 7;
+        public Form1()
+        {
+            InitializeComponent();
+            trayIcon.Visible = true;
+        }
+
+        private void NotifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ShouldShowFlyout())
+            {
+                ShowFlyout();
+            }
+        }
+
+        private bool ShouldShowFlyout()
+        {
+            if (isFirstExecution)
+            {
+                lastExecutionTime = DateTime.Now;
+                isFirstExecution = false;
+                return true;
+            }
+            else
+            {
+                TimeSpan timeSinceLastExecution = DateTime.Now - lastExecutionTime;
+                if (timeSinceLastExecution.TotalSeconds >= 5)
+                {
+                    lastExecutionTime = DateTime.Now;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ShowFlyout()
+        {
+            if (isFlyoutVisible) return;
+
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+            ToolStripTextBox textBox = CreateToolStripTextBox();
+            trayIcon.ContextMenuStrip.Items.Add(textBox);
+
+            trayIcon.ContextMenuStrip.Show(Control.MousePosition);
+            isFlyoutVisible = true;
+            StartFlyoutClosingTimer();
+        }
+
+        private ToolStripTextBox CreateToolStripTextBox()
+        {
+            ToolStripTextBox textBox = new ToolStripTextBox
+            {
+                ReadOnly = true,
+                AutoSize = false,
+                Multiline = true,
+                Enabled = false, // Benutzer kann nicht hineinklicken
+                Font = new Font(Font.FontFamily, FontSize) // Ändern der Schriftgröße auf 12
+            };
+
+            int AnzahlZeilen = 0;
+            try
+            {
+                string[] lines = File.ReadAllLines(strFile, Encoding.GetEncoding("iso-8859-1"));
+                AnzahlZeilen = lines.Length;
+                string content = "";
+                for (int i = 0; i < lines.Length && i < AnzahlZeilen; i++)
+                {
+                    content += lines[i] + Environment.NewLine;
+                }
+                textBox.Text = content;
+                AnzahlZeilen = File.ReadAllLines(strFile).Length;
+            }
+            catch (Exception ex)
+            {
+                textBox.Text = "Fehler beim Lesen der Datei: " + ex.Message;
+            }
+
+            // Berechne die TextBox-Höhe basierend auf der Anzahl der Zeilen und der Schriftgröße
+            int lineHeight = TextRenderer.MeasureText("A", textBox.Font).Height;
+            textBox.Height = (lineHeight * AnzahlZeilen) + (FontSize * 2) ;
+            textBox.Width = 800;
+
+            return textBox;
+        }
+
+        private void StartFlyoutClosingTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 5000;
+            timer.Tick += (s, args) =>
+            {
+                trayIcon.ContextMenuStrip.Close();
+                isFlyoutVisible = false;
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.Start();
+        }
+
+        private void trayIcon_MouseEnter(object sender, EventArgs e)
+        {
+            Console.WriteLine("Closing Application");
+            Application.Exit();
+        }
+
+        private void cmd_SelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Path.GetDirectoryName(strFile),
+                Filter = "Textdateien (*.txt)|*.txt|Alle Dateien (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFile = openFileDialog.FileName;
+                txt_Path.Text = strFile;
+            }
+        }
+
+    }
+}
